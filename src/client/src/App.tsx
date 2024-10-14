c3b4ba5074e5a4cd239efca179cd95dc0f457a4e
CaptainCluster <sranta.ville@gmail.com>
2024-10-15 02:42:19 +0300

    further planning

--- src/client/src/App.tsx
+++ src/client/src/App.tsx
@@ -1,34 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import FrontPage from './pages/FrontPage' 
+import Skills from './pages/Skills'
+import Projects from './pages/Projects'
+import Error404 from './pages/Error404'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<FrontPage />}/>
+        <Route path="*" element={<Error404 />}/>
+        <Route path="skills" element={<Skills />}/>
+        <Route path="projects" element={<Projects />}/>
+      </Routes>
+    </Router>
   )
 }
 
