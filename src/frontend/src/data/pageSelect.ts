8de0f1c45fb4a9a218ad29f7360dfec6a46cfd62
CaptainCluster <sranta.ville@gmail.com>
2024-10-25 07:50:36 +0300

    Early styling, about me

---
+++ src/frontend/src/data/pageSelect.ts
@@ -0,0 +1,19 @@
+import PageSelectInterface from "../interfaces/PageSelectInterface";
+
+export const selectablePages: PageSelectInterface[] = [
+  {
+    pageTitle:  "About Me",
+    pageBio:    "Learn more about me, in general.",
+    pageUrl:    "/about"
+  },
+  {
+    pageTitle:  "Projects",
+    pageBio:    "Here are some of my notable projects.",
+    pageUrl:    "/projects"
+  },
+  {
+    pageTitle:  "Skills",
+    pageBio:    "Here are some of my software engineering skills.",
+    pageUrl:    "/skills"
+  }
+] 
