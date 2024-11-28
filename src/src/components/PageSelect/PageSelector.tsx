ae2c2b021109487fe565dd12a492d7abfe078064
CaptainCluster <sranta.ville@gmail.com>
2024-11-28 15:38:22 +0200

    Looks for four sections

--- src/src/components/PageSelect/PageSelector.tsx
+++ src/src/components/PageSelect/PageSelector.tsx
@@ -22,7 +22,7 @@ const createSelectablePage = ({pageTitle, pageBio, pageUrl}: PageSelectInterface
 const PageSelector = () => {
   return(
     <div className="grid items-center justify-center h-screen bg-gray-100 fade-in-component">
-      <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6 p-6">
+      <div className="grid grid-cols-1 sm:grid-cols-2 gap-6 p-6"> 
         {selectablePages.length === 0 
           ? <p>No pages available</p>
           : selectablePages.map((selectablePage, index) => createSelectablePage(selectablePage, index))
