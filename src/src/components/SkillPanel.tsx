bd728c383c6803dea3668bae7148603ce32793bf
CaptainCluster <sranta.ville@gmail.com>
2024-12-20 17:45:02 +0200

    Padding and font for skill entries

--- src/src/components/SkillPanel.tsx
+++ src/src/components/SkillPanel.tsx
@@ -3,10 +3,10 @@ import { skillPanelData } from "../data/skillpanel";
 const SkillPanel = ({skillName}: {skillName: string}) => {
   const DEFAULT_TEXT: string = "No description defined for this skill.";
   return (
-    <div className="slide-animation">
+    <div className="slide-animation font-serif">
       {skillPanelData[skillName] === undefined  
-        ? <p className="fade-in-text">{DEFAULT_TEXT}</p>
-        : skillPanelData[skillName].map((paragraph, index) => <p className="fade-in-component" key={index}>{paragraph}</p>) 
+        ? <p className="fade-in-text pt-3">{DEFAULT_TEXT}</p>
+        : skillPanelData[skillName].map((paragraph, index) => <p className="fade-in-component pt-3" key={index}>{paragraph}</p>) 
       }
     </div>
   )
