72bd7785e7fd0602bba0d60c117d917dae30596f
CaptainCluster <sranta.ville@gmail.com>
2025-01-10 15:21:26 +0200

    Badges for projects

--- src/src/pages/Projects/ProjectEntry.tsx
+++ src/src/pages/Projects/ProjectEntry.tsx
@@ -1,3 +1,4 @@
+import SkillBadgeHolder from "../../components/SkillBadgeHolder";
 import ProjectEntryInterface    from "../../models/interfaces/ProjectEntryInterface";
 import { useState, useEffect }  from "react";
 
@@ -12,14 +13,13 @@ const ProjectEntry = ({ name, images, technologies, status, description, links }
   }, []);
 
   const imgDirectory: string = "../src/assets/"
-
   return (
     <div className="font-serif text-center bg-white rounded-lg shadow-lg transition-transform hover:scale-105">
       <div className="p-5">
         <h1 className="text-2xl">{name}</h1>
         <div className="p-3">
           <h2 className="text-xl">Technologies used</h2>
-          {technologies.map((technologyEntry) => (<p>{technologyEntry}</p>))}
+          <SkillBadgeHolder skills={technologies}/>
         </div>
         <div className="p-3">
           <h2 className="text-xl">Status</h2>
@@ -32,8 +32,8 @@ const ProjectEntry = ({ name, images, technologies, status, description, links }
       </div>
       <div>
         {description.map((descriptionEntry) => (<p className="p-3">{descriptionEntry}</p>))}
-        <div className="mb-5">
-          {images.map((imageEntry) => (<img className="p-5" src={imgDirectory + imageEntry.src} alt={imageEntry.alt}/>))}
+        <div className="mb-5 p-5">
+          {images.map((imageEntry) => (<img className="rounded-lg" src={imgDirectory + imageEntry.src} alt={imageEntry.alt}/>))}
         </div>
       </div> 
     </div>
