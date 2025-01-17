14ab1e9e9e22f1feede5a526806e19454b3bdc1f
CaptainCluster <sranta.ville@gmail.com>
2025-01-17 20:07:38 +0200

    Utilization of back button

--- src/src/components/BackButton.tsx
+++ src/src/components/BackButton.tsx
@@ -1,6 +1,10 @@
 const BackButton = () => {
   return (
-    <button className="font-serif ml-2 mt-2" onClick={() => { window.location.href="/" }}>Back to Front Page</button>
+    <button 
+      className="p-2 font-serif ml-2 mt-2 bg-white rounded-lg shadow-lg transform transition-transform hover:scale-105" 
+      onClick={() => { window.location.href="/"}}>
+      {"<-- Back to Front Page"}
+    </button>
   )
 }
 
--- src/src/pages/Experience/Experience.tsx
+++ src/src/pages/Experience/Experience.tsx
@@ -1,3 +1,4 @@
+import BackButton from "../../components/BackButton";
 import PageTitle          from "../../components/PageName";
 import { experienceData } from "../../data/experience";
 import ExperienceEntry    from "./ExperienceEntry"; 
@@ -5,6 +6,7 @@ import ExperienceEntry    from "./ExperienceEntry";
 const Experience = () => {
   return (
     <div className="projects-page h-screen bg-gray-100 fade-in overflow-scroll">
+      <BackButton />
       <PageTitle pageTitle="Experience"/>
       <div className="grid grid-cols-1 gap-10 p-10 max-w-7xl mx-auto">
         {experienceData.length === 0 
