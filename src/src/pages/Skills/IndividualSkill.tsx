14470b5d1cf619212fcd767b4057cdb5fc8cbecd
CaptainCluster <sranta.ville@gmail.com>
2025-01-17 19:42:48 +0200

    Skill text expands to details

--- src/src/pages/Skills/IndividualSkill.tsx
+++ src/src/pages/Skills/IndividualSkill.tsx
@@ -18,8 +18,10 @@ const IndividualSkill = ({skillName, skillProficiency}: {skillName: string, skil
     <div className="grid mt-2 mb-2">
        <div className="flex items-center justify-center"> 
         <div className="w-2/5">
-          <div className="mb-1 text-lg font-serif text-center">{skillName}</div>
-          <div className="w-full h-6 bg-gray-200 rounded-full dark:bg-gray-700" onMouseEnter={() => setHoveredOver(true)} onMouseLeave={() => setHoveredOver(false)}> 
+          <div className="mb-1 text-lg font-serif text-center" 
+            onMouseEnter={() => setHoveredOver(true)} 
+            onMouseLeave={() => setHoveredOver(false)}>{skillName}</div>
+          <div className="w-full h-6 bg-gray-200 rounded-full dark:bg-gray-700"> 
             <div className="h-6 bg-blue-600 rounded-full dark:bg-blue-500" style={{ width: proficiencyFormatted }}></div> 
           </div> 
           {hoveredOver && <SkillPanel skillName={skillName.toLowerCase()} />}
